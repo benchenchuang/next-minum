4a69c39dc29b164868b4c69fae346fffc30b6e39
benchenchuang <benchenchuang>
2023-12-24 10:25:20 +0800

    update

--- src/components/SiteFooter/index.tsx
+++ src/components/SiteFooter/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <>
-            <div className='hidden sm:flex'>pc - Footer-test</div>
+            <div className='hidden sm:flex'>pc - Footer</div>
             <div className='sm:hidden'>phone-Footer-test</div>
         </>
     )
